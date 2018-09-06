79da25ea2308ec711cc27c7dcad9f90367d8c349
Anuk Shakya <i11134@d2hs.com>
2018-09-06 03:48:04 -0700

    user added

--- src/app/components/user/view-user/view-user.component.ts
+++ src/app/components/user/view-user/view-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../../models/User';
+import { UserService } from '../../../services/userService/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-user',
@@ -7,9 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewUserComponent implements OnInit {
 
-  constructor() { }
+  private users: User[];
+
+  constructor(private userService: UserService, private router: Router) { }
+
 
   ngOnInit() {
+    this.userService.getUsers().subscribe((users: any) => {
+      console.log(users);
+      this.users = users;
+    }, (error) => {
+      console.log(error);
+    })
+ 
   }
 
+  deleteRecord(user) {
+    this.userService.deleteUser(user.userId).subscribe(() => {
+      this.users.splice(this.users.indexOf(user), 1);
+    }, (error) => {
+      console.log(error);
+
+    });
+  }
+
+
+  updateRecord(user) {
+    this.userService.setter(user);
+    this.router.navigate(['/addblood']);
+  }
+
+  newRecord() {
+    let user = new User();
+    this.userService.setter(user);
+    this.router.navigate(['/addblood']);
+
+  }
 }
+
+
